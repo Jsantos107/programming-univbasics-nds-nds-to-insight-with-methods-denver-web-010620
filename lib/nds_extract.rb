4833af6dd16cb86f8b0cb2c05d830d0cbad02daa
Jorge Santos <neneluis55@gmail.com>
2019-12-30 08:44:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-
+require 'yaml'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 
